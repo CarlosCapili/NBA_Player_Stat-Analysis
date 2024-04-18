8bdb38ddf3ea2a6da20468417f0b8172f5b6d077
Carlos Capili <carloscapili21@gmail.com>
2024-04-18 00:04:20 -0400

    Added average games played per season query

--- main.sql
+++ main.sql
@@ -21,92 +21,112 @@ SELECT
 FROM ordered_age
 GROUP BY season;
 
-
--- PERFORMANCE METRICS
-
--- Player with the highest ppg per season from 2000 and up
-WITH ordered_ppg AS (
-	SELECT
-		player_name,
-		country,
-		pts,
-		season,
-		ROW_NUMBER() OVER(PARTITION BY season ORDER BY pts DESC) AS rn
-	FROM nba_data
-	WHERE season LIKE '2___-__'
-)
-
+-- What is the average number of games played by an NBA player per season? (What are the percentage increase/decline between season)
+-- simplify this query too hard to read. maybe make it into a CTE.
 SELECT
-	player_name,
-	country,
-	pts,
-	season
-FROM ordered_ppg
-WHERE rn = 1;
-
--- Player with the highest rpg per season from 2000 and up
-WITH ordered_rpg AS (
-	SELECT
-		player_name,
-		country,
-		reb,
-		season,
-		ROW_NUMBER() OVER(PARTITION BY season ORDER BY reb DESC) AS rn
-	FROM nba_data
-	WHERE season LIKE '2___-__'
-)
-
-SELECT
-	player_name,
-	country,
-	reb,
-	season
-FROM ordered_rpg
-WHERE rn = 1;
-
--- Player with the highest apg per season from 2000 and up
-WITH ordered_apg AS (
-	SELECT
-		player_name,
-		country,
-		ast,
-		season,
-		ROW_NUMBER() OVER(PARTITION BY season ORDER BY ast DESC) AS rn
-	FROM nba_data
-	WHERE season LIKE '2___-__'
-)
-
-SELECT
-	player_name,
-	country,
-	ast,
-	season
-FROM ordered_apg
-WHERE rn = 1;
-
--- Who are the top 10 most efficient scorers in 2022-23 season? (Who has the best true shooting percentage?)
-SELECT
-	player_name,
-	team_abbrev,
-	country,
-	(ts_pct * 100.0) AS true_shooting_pct
+	season,
+	ROUND(AVG(gp), 1) AS avg_gp,
+	LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season) AS previous_avg_gp,
+	ROUND(AVG(gp), 1) - (LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season)) AS inc_dec,
+	CASE
+		WHEN (ROUND(AVG(gp), 1) - (LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season))) < 0 
+			THEN CONCAT(ROUND(ABS(ROUND(AVG(gp), 1) - (LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season))) / LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season) * 100.0, 2), '% decrease')
+			
+		WHEN (ROUND(AVG(gp), 1) - (LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season))) > 0 
+			THEN CONCAT(ROUND(ABS(ROUND(AVG(gp), 1) - (LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season))) / LAG(ROUND(AVG(gp), 1)) OVER(ORDER BY season) * 100.0, 2), '% increase')
+			
+	END AS pct_inc_dec
 FROM nba_data
-WHERE season = '2022-23'
-ORDER BY ts_pct DESC
-LIMIT 10;
+WHERE season LIKE '2___-__'
+GROUP BY season;
+
+-- What is the average height of an NBA player per season?
+
+-- -- PERFORMANCE METRICS
+
+-- -- Player with the highest ppg per season from 2000 and up
+-- WITH ordered_ppg AS (
+-- 	SELECT
+-- 		player_name,
+-- 		country,
+-- 		pts,
+-- 		season,
+-- 		ROW_NUMBER() OVER(PARTITION BY season ORDER BY pts DESC) AS rn
+-- 	FROM nba_data
+-- 	WHERE season LIKE '2___-__'
+-- )
+
+-- SELECT
+-- 	player_name,
+-- 	country,
+-- 	pts,
+-- 	season
+-- FROM ordered_ppg
+-- WHERE rn = 1;
+
+-- -- Player with the highest rpg per season from 2000 and up
+-- WITH ordered_rpg AS (
+-- 	SELECT
+-- 		player_name,
+-- 		country,
+-- 		reb,
+-- 		season,
+-- 		ROW_NUMBER() OVER(PARTITION BY season ORDER BY reb DESC) AS rn
+-- 	FROM nba_data
+-- 	WHERE season LIKE '2___-__'
+-- )
+
+-- SELECT
+-- 	player_name,
+-- 	country,
+-- 	reb,
+-- 	season
+-- FROM ordered_rpg
+-- WHERE rn = 1;
+
+-- -- Player with the highest apg per season from 2000 and up
+-- WITH ordered_apg AS (
+-- 	SELECT
+-- 		player_name,
+-- 		country,
+-- 		ast,
+-- 		season,
+-- 		ROW_NUMBER() OVER(PARTITION BY season ORDER BY ast DESC) AS rn
+-- 	FROM nba_data
+-- 	WHERE season LIKE '2___-__'
+-- )
+
+-- SELECT
+-- 	player_name,
+-- 	country,
+-- 	ast,
+-- 	season
+-- FROM ordered_apg
+-- WHERE rn = 1;
+
+-- -- Who are the top 10 most efficient scorers in 2022-23 season? (Who has the best true shooting percentage?)
+-- SELECT
+-- 	player_name,
+-- 	team_abbrev,
+-- 	country,
+-- 	(ts_pct * 100.0) AS true_shooting_pct
+-- FROM nba_data
+-- WHERE season = '2022-23'
+-- ORDER BY ts_pct DESC
+-- LIMIT 10;
 	
--- Top 10 players who average a double-double in 2022-23 season (pts, rebs, ast, no steals or blocks are in dataset)
-SELECT
-	player_name,
-	team_abbrev,
-	pts,
-	reb,
-	ast
-FROM nba_data
-WHERE 
-	season = '2022-23'
-	AND ((pts >= 10 AND reb >= 10) OR (pts >= 10 AND ast >= 10) OR (reb >= 10 AND ast >= 10))
-ORDER BY pts, reb, ast
-LIMIT 10;
+-- -- Top 10 players who average a double-double in 2022-23 season (pts, rebs, ast, no steals or blocks are in dataset)
+-- SELECT
+-- 	player_name,
+-- 	team_abbrev,
+-- 	pts,
+-- 	reb,
+-- 	ast
+-- FROM nba_data
+-- WHERE 
+-- 	season = '2022-23'
+-- 	AND ((pts >= 10 AND reb >= 10) OR (pts >= 10 AND ast >= 10) OR (reb >= 10 AND ast >= 10))
+-- ORDER BY pts, reb, ast
+-- LIMIT 10;
 
 
